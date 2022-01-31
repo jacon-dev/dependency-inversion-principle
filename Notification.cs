ac97f2274f24b1a8e17045184f4203ce2ef79a5a
James Conlin <jacon.dev@outlook.com>
2022-01-31 16:25:17 +0000

    initial commit

---
+++ Notification.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DependencyInversion
+{
+    public class Notification
+    {
+        private Email _email;
+        private Sms _sms;
+
+        public Notification()
+        {
+            _email = new Email();
+            _sms = new Sms();
+        }
+
+        public void SendNotification()
+        {
+            _email.SendEmail();
+            _sms.SendSms();
+        }
+    }
+    
+    public class Email
+    {
+        public string ToAddress { get; set; }
+        public string FromAddress { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public void SendEmail()
+        {
+            //Send the email
+        }
+    }
+
+    public class Sms
+    {
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+
+        public void SendSms()
+        {
+            //Send the SMS
+        }
+    }
+}
